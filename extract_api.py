560921c40580617a82ab594935faf9800e637327
Xuan Truong <truongnx@Xuans-MacBook-Pro.local>
2023-02-16 01:08:04 +0700

    update

--- extract_api.py
+++ extract_api.py
@@ -11,7 +11,7 @@ from backend import extract_data
 
 ### args
 parser = argparse.ArgumentParser()
-parser.add_argument('--port',type=int, default=5000)
+parser.add_argument('-p','--port',type=int, default=5000, help="port number")
 args = parser.parse_args()
 
 ###logging
